4f79268e05003c24a566b37a6152f9c269aeaaa8
김승우 <tmd8633@naver.com>
2024-09-16 19:17:59 +0900

    feat : json 경로변경

--- src/main/java/ban/inspector/updater/WordPathConstant.java
+++ src/main/java/ban/inspector/updater/WordPathConstant.java
@@ -4,6 +4,6 @@ public abstract class WordPathConstant {
 
     private WordPathConstant() {}
 
-    public static final String BAN_WORD_PATH = "../static/BanWords.json";
-    public static final String EXCEPT_WORD_PATH = "../static/ExceptWords.json";
+    public static final String BAN_WORD_PATH = "static/BanWords.json";
+    public static final String EXCEPT_WORD_PATH = "static/ExceptWords.json";
 }
